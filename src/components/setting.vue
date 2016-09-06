6ed29fcd7afdf2b93dd78b121e9cf2ef648e552b
Lee <dragon8github@github,com>
2016-09-06 21:49:29 +0800

    准备开启新的分支、使用mui的DIV切换

--- src/components/setting.vue
+++ src/components/setting.vue
@@ -1,145 +1,85 @@
+
 <template>
 	<div id="setting"  class="mui-control-content">
 		<header class="mui-bar mui-bar-nav">
 			<h1 class="mui-title">设置</h1>
 		</header>
-		<div class="mui-content">
+		<div class="mui-content" style="background:#FFF">
 			<div class="title">
-				七个月挑战
+				一般锻炼
 			</div>
 			<ul class="mui-table-view">
 				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="../../static/images/shuijiao.jpg">
+					<a href="javascript:;" class="mui-navigate-right">
+						<img class="mui-media-object mui-pull-left" src="../../static/images/tixing.png">
 						<div class="mui-media-body">
-							幸福
-							<p class='mui-ellipsis'>能和心爱的人一起睡觉，是件幸福的事情；可是，打呼噜怎么办？</p>
+							提醒
+						</div>
+					</a>
+				</li>
+				<li class="mui-table-view-cell mui-media">
+					<a href="javascript:;" class="mui-navigate-right">
+						<img class="mui-media-object mui-pull-left" src="../../static/images/yinyue.png">
+						<div class="mui-media-body">
+							声音
 						</div>
 					</a>
 				</li>
 				<li class="mui-table-view-cell mui-media">
 					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="../../static/images/muwu.jpg">
+						<img class="mui-media-object mui-pull-left" height="38px" src="../../static/images/zhendong.png">
 						<div class="mui-media-body">
-							木屋
-							<p class='mui-ellipsis'>想要这样一间小木屋，夏天挫冰吃瓜，冬天围炉取暖.</p>
+							震动
 						</div>
 					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="../../static/images/cbd.jpg">
-						<div class="mui-media-body">
-							CBD
-							<p class='mui-ellipsis'>烤炉模式的城，到黄昏，如同打翻的调色盘一般.</p>
-						</div>
-					</a>
+					<div class="mui-switch mui-active mui-switch-mini" data-switch="9">
+						<div class="mui-switch-handle"></div>
+					</div>
 				</li>
 			</ul>
 			<div class="title">
-				连续天数
+				锻炼
 			</div>
 			<ul class="mui-table-view">
 				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="../../static/images/shuijiao.jpg">
+					<a href="javascript:;" class="mui-navigate-right">
+						<img class="mui-media-object mui-pull-left" src="../../static/images/jiankang.png">
 						<div class="mui-media-body">
-							幸福
-							<p class='mui-ellipsis'>能和心爱的人一起睡觉，是件幸福的事情；可是，打呼噜怎么办？</p>
+							健康
+						</div>
+					</a>
+				</li>
+				<li class="mui-table-view-cell mui-media">
+					<a href="javascript:;" class="mui-navigate-right">
+						<img class="mui-media-object mui-pull-left" src="../../static/images/jiange.png">
+						<div class="mui-media-body">
+							间隔
 						</div>
 					</a>
 				</li>
 				<li class="mui-table-view-cell mui-media">
 					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="../../static/images/muwu.jpg">
+						<img class="mui-media-object mui-pull-left" src="../../static/images/suiji.png">
 						<div class="mui-media-body">
-							木屋
-							<p class='mui-ellipsis'>想要这样一间小木屋，夏天挫冰吃瓜，冬天围炉取暖.</p>
+							随机
 						</div>
 					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="../../static/images/cbd.jpg">
-						<div class="mui-media-body">
-							CBD
-							<p class='mui-ellipsis'>烤炉模式的城，到黄昏，如同打翻的调色盘一般.</p>
-						</div>
-					</a>
+					<div class="mui-switch mui-active mui-switch-mini" data-switch="9">
+						<div class="mui-switch-handle"></div>
+					</div>
 				</li>
 			</ul>
-			<div class="title">
-				已结束循环
+			<div id="web_info">
+				<p >企业培训 | 合作事宜 | 版权投诉</p>
+				<p>粤ICP备12345678. © 1993-2016 程序员在囧途. Powered by CyLee.</p>
 			</div>
-			<ul class="mui-table-view">
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="../../static/images/shuijiao.jpg">
-						<div class="mui-media-body">
-							幸福
-							<p class='mui-ellipsis'>能和心爱的人一起睡觉，是件幸福的事情；可是，打呼噜怎么办？</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="../../static/images/muwu.jpg">
-						<div class="mui-media-body">
-							木屋
-							<p class='mui-ellipsis'>想要这样一间小木屋，夏天挫冰吃瓜，冬天围炉取暖.</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="../../static/images/cbd.jpg">
-						<div class="mui-media-body">
-							CBD
-							<p class='mui-ellipsis'>烤炉模式的城，到黄昏，如同打翻的调色盘一般.</p>
-						</div>
-					</a>
-				</li>
-			</ul>
-			<div class="title">
-				锻炼时间
-			</div>
-			<ul class="mui-table-view">
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="../../static/images/shuijiao.jpg">
-						<div class="mui-media-body">
-							幸福
-							<p class='mui-ellipsis'>能和心爱的人一起睡觉，是件幸福的事情；可是，打呼噜怎么办？</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="../../static/images/muwu.jpg">
-						<div class="mui-media-body">
-							木屋
-							<p class='mui-ellipsis'>想要这样一间小木屋，夏天挫冰吃瓜，冬天围炉取暖.</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="../../static/images/cbd.jpg">
-						<div class="mui-media-body">
-							CBD
-							<p class='mui-ellipsis'>烤炉模式的城，到黄昏，如同打翻的调色盘一般.</p>
-						</div>
-					</a>
-				</li>
-			</ul>
-		</div>
-		<div id="web_info">
-			<p >企业培训 | 合作事宜 | 版权投诉</p>
-			<p>粤ICP备12345678. © 1993-2016 程序员在囧途. Powered by CyLee.</p>
 		</div>
 	</div>
 </template>
 <style>
 	#web_info{text-align: center;margin:50px auto 100px;}
+	#setting .mui-media-body{padding-top:8px;}
+	#setting .mui-table-view-cell>a:not(.mui-btn) {margin:-13px -15px;}
+	#setting .mui-table-view .mui-media-object img{height:auto;}
 </style>
 
