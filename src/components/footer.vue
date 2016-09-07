df85fde4e7fbc56f097449e69a1076dfd74afa7e
Lee <dragon8github@github,com>
2016-09-07 22:30:22 +0800

    准备开始新分支

---
+++ src/components/footer.vue
@@ -0,0 +1,28 @@
+<template>
+	<div id="footer">
+	<nav class="mui-bar mui-bar-tab">
+		<a class="mui-tab-item mui-active" href="#home">
+			<span class="mui-icon"><i class="icon iconfont">&#xe603;</i></span>
+			<span class="mui-tab-label">锻炼</span>
+		</a>
+		<a class="mui-tab-item"  href="#study">
+			<span class="mui-icon"><i class="icon iconfont">&#xe604;</i></span>
+			<span class="mui-tab-label">学习</span>
+		</a>
+		<a class="mui-tab-item" href="#achieve">
+			<span class="mui-icon"><i class="icon iconfont">&#xe601;</i></span>
+			<span class="mui-tab-label">成就</span>
+		</a>
+		<a class="mui-tab-item" href="#setting">
+			<span class="mui-icon"><i class="icon iconfont">&#xe608;</i></span>
+			<span class="mui-tab-label">设置</span>
+		</a>
+	</nav>
+</div>
+</template>
+
+
+
+<style>
+	#footer .mui-active .mui-icon, #footer .mui-active .mui-tab-label{color:tan;} 
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,27 +4,7 @@
 		<study></study>
 		<achieve></achieve>
 		<setting></setting>
-
-		<div id="footer">
-			<nav class="mui-bar mui-bar-tab">
-				<a class="mui-tab-item mui-active" href="#home">
-					<span class="mui-icon"><i class="icon iconfont">&#xe603;</i></span>
-					<span class="mui-tab-label">锻炼</span>
-				</a>
-				<a class="mui-tab-item"  href="#study">
-					<span class="mui-icon"><i class="icon iconfont">&#xe604;</i></span>
-					<span class="mui-tab-label">学习</span>
-				</a>
-				<a class="mui-tab-item" href="#achieve">
-					<span class="mui-icon"><i class="icon iconfont">&#xe601;</i></span>
-					<span class="mui-tab-label">成就</span>
-				</a>
-				<a class="mui-tab-item" href="#setting">
-					<span class="mui-icon"><i class="icon iconfont">&#xe608;</i></span>
-					<span class="mui-tab-label">设置</span>
-				</a>
-			</nav>
-		</div>
+		<myfooter></myfooter>
 	</div>
 </template>
 
@@ -33,17 +13,17 @@
 	import study from './../components/study'
 	import achieve from './../components/achieve'
 	import setting from './../components/setting'
+	import myfooter from './../components/footer'
+
+	console.log(myfooter)
 
 	export default {
 		components: {
 			home,
 			study,
 			achieve,
-			setting
+			setting,
+			myfooter
 		}
 	}	
 </script>
-
-<style>
-	#footer .mui-active .mui-icon, #footer .mui-active .mui-tab-label{color:tan;} 
-</style>
