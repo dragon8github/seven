9049356ac272005c0073c8fd4a99c5f2fe1741cd
Lee <dragon8github@github,com>
2016-09-08 22:25:19 +0800

    继续检查

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
+	<div style="height: 100vh;">
 		<tab1></tab1>
 		<tab2></tab2>
 		<tab3></tab3>
--- src/components/tab3/index.vue
+++ src/components/tab3/index.vue
@@ -4,290 +4,302 @@
 			<h1 class="mui-title">成就</h1>
 		</header>
 		<div class="mui-content" style="margin-bottom:100px;background:#fff" >
-			<div class="title">
-				七个月挑战
+			<div class="mui-scroll-wrapper">
+				<div class="mui-scroll">
+					<div class="title" style="margin-top:70px">
+						七个月挑战
+					</div>
+					<ul class="mui-table-view">
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/cxin.jpg">
+								<div class="mui-media-body">
+									不碎的心
+									<p class='mui-ellipsis'>一个月错过少于三个锻炼日可恢复你的心数</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/cri3.jpg">
+								<div class="mui-media-body">
+									养成一个习惯
+									<p class='mui-ellipsis'>坚持挑战3个月</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/c6.jpg">
+								<div class="mui-media-body">
+									接近目标
+									<p class='mui-ellipsis'>坚持挑战6个月</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/c7.jpg">
+								<div class="mui-media-body">
+									七个月冠军
+									<p class='mui-ellipsis'>完成七个月的挑战</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/c12.jpg">
+								<div class="mui-media-body">
+									超越极限
+									<p class='mui-ellipsis'>完成挑战12个月</p>
+								</div>
+							</a>
+						</li>
+					</ul>
+					<div class="title">
+						连续天数
+					</div>
+					<ul class="mui-table-view">
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/cdui.jpg">
+								<div class="mui-media-body">
+									接受挑战
+									<p class='mui-ellipsis'>完成你第一个训练</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/czao3.jpg">
+								<div class="mui-media-body">
+									帽子戏法
+									<p class='mui-ellipsis'>连续锻炼3天.</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/czao7.jpg">
+								<div class="mui-media-body">
+									壹周秀
+									<p class='mui-ellipsis'>连续强烈运动七天.</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/czao14.jpg">
+								<div class="mui-media-body">
+									这是一个挑战吗？
+									<p class='mui-ellipsis'>连续强烈运动2周.</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/c1.jpg">
+								<div class="mui-media-body">
+									坚持一个月
+									<p class='mui-ellipsis'>每天锻炼坚持一个月.</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/c3.jpg">
+								<div class="mui-media-body">
+									季度表现良好
+									<p class='mui-ellipsis'>每天锻炼坚持3个月.</p>
+								</div>
+							</a>
+						</li>
+					</ul>
+					<div class="title">
+						已结束循环
+					</div>
+					<ul class="mui-table-view">
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/c2.jpg">
+								<div class="mui-media-body">
+									运动员水准
+									<p class='mui-ellipsis'>一天完成2组循环</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/cxuan3.jpg">
+								<div class="mui-media-body">
+									3是个好数字
+									<p class='mui-ellipsis'>一天完成3组循环.</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/c5.jpg">
+								<div class="mui-media-body">
+									40分钟之疼
+									<p class='mui-ellipsis'>一天完成5组循环.</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/c10.jpg">
+								<div class="mui-media-body">
+									无限之舞
+									<p class='mui-ellipsis'>一天完成10组循环.</p>
+								</div>
+							</a>
+						</li>
+					</ul>
+					<div class="title">
+						锻炼时间
+					</div>
+					<ul class="mui-table-view">
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/cniao.jpg">
+								<div class="mui-media-body">
+									早起的鸟儿
+									<p class='mui-ellipsis'>在上午4时至7时之间完成一个锻炼</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/cwu.jpg">
+								<div class="mui-media-body">
+									下午茶
+									<p class='mui-ellipsis'>在下午12时至4时之间完成一个锻炼.</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/cwan.jpg">
+								<div class="mui-media-body">
+									夜猫子
+									<p class='mui-ellipsis'>在晚上6点之后完成一个锻炼.</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/criyue.jpg">
+								<div class="mui-media-body">
+									夜以继日
+									<p class='mui-ellipsis'>在同一天三个时间段都完成锻炼.</p>
+								</div>
+							</a>
+						</li>
+					</ul>
+					<div class="title">
+						已解锁锻炼
+					</div>
+					<ul class="mui-table-view" style="margin-bottom:100px">
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/cshangbanshen.jpg">
+								<div class="mui-media-body">
+									上半身锻炼
+									<p class='mui-ellipsis'>解锁上半身锻炼</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/cquanshen.jpg">
+								<div class="mui-media-body">
+									核心部位训练
+									<p class='mui-ellipsis'>解锁核心部位训练.</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/cxiabanshen.jpg">
+								<div class="mui-media-body">
+									下半身锻炼
+									<p class='mui-ellipsis'>解锁下半身锻炼.</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/czhifan.jpg">
+								<div class="mui-media-body">
+									心肺运动
+									<p class='mui-ellipsis'>解锁心肺运动.</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/cxinfei.jpg">
+								<div class="mui-media-body">
+									脂肪燃烧
+									<p class='mui-ellipsis'>解锁脂肪燃烧.</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/cbaozha.jpg">
+								<div class="mui-media-body">
+									爆发式训练
+									<p class='mui-ellipsis'>解锁爆发式训练.</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/cliliang.jpg">
+								<div class="mui-media-body">
+									力量训练
+									<p class='mui-ellipsis'>解锁力量训练.</p>
+								</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media">
+							<a href="javascript:;">
+								<img class="mui-media-object mui-pull-left" src="./../../../static/images/cshenzhan.jpg">
+								<div class="mui-media-body">
+									伸展运动
+									<p class='mui-ellipsis'>解锁伸展运动.</p>
+								</div>
+							</a>
+						</li>
+					</ul>
+				</div>
 			</div>
-			<ul class="mui-table-view">
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/cxin.jpg">
-						<div class="mui-media-body">
-							不碎的心
-							<p class='mui-ellipsis'>一个月错过少于三个锻炼日可恢复你的心数</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/cri3.jpg">
-						<div class="mui-media-body">
-							养成一个习惯
-							<p class='mui-ellipsis'>坚持挑战3个月</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/c6.jpg">
-						<div class="mui-media-body">
-							接近目标
-							<p class='mui-ellipsis'>坚持挑战6个月</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/c7.jpg">
-						<div class="mui-media-body">
-							七个月冠军
-							<p class='mui-ellipsis'>完成七个月的挑战</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/c12.jpg">
-						<div class="mui-media-body">
-							超越极限
-							<p class='mui-ellipsis'>完成挑战12个月</p>
-						</div>
-					</a>
-				</li>
-			</ul>
-			<div class="title">
-				连续天数
-			</div>
-			<ul class="mui-table-view">
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/cdui.jpg">
-						<div class="mui-media-body">
-							接受挑战
-							<p class='mui-ellipsis'>完成你第一个训练</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/czao3.jpg">
-						<div class="mui-media-body">
-							帽子戏法
-							<p class='mui-ellipsis'>连续锻炼3天.</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/czao7.jpg">
-						<div class="mui-media-body">
-							壹周秀
-							<p class='mui-ellipsis'>连续强烈运动七天.</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/czao14.jpg">
-						<div class="mui-media-body">
-							这是一个挑战吗？
-							<p class='mui-ellipsis'>连续强烈运动2周.</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/c1.jpg">
-						<div class="mui-media-body">
-							坚持一个月
-							<p class='mui-ellipsis'>每天锻炼坚持一个月.</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/c3.jpg">
-						<div class="mui-media-body">
-							季度表现良好
-							<p class='mui-ellipsis'>每天锻炼坚持3个月.</p>
-						</div>
-					</a>
-				</li>
-			</ul>
-			<div class="title">
-				已结束循环
-			</div>
-			<ul class="mui-table-view">
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/c2.jpg">
-						<div class="mui-media-body">
-							运动员水准
-							<p class='mui-ellipsis'>一天完成2组循环</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/cxuan3.jpg">
-						<div class="mui-media-body">
-							3是个好数字
-							<p class='mui-ellipsis'>一天完成3组循环.</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/c5.jpg">
-						<div class="mui-media-body">
-							40分钟之疼
-							<p class='mui-ellipsis'>一天完成5组循环.</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/c10.jpg">
-						<div class="mui-media-body">
-							无限之舞
-							<p class='mui-ellipsis'>一天完成10组循环.</p>
-						</div>
-					</a>
-				</li>
-			</ul>
-			<div class="title">
-				锻炼时间
-			</div>
-			<ul class="mui-table-view">
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/cniao.jpg">
-						<div class="mui-media-body">
-							早起的鸟儿
-							<p class='mui-ellipsis'>在上午4时至7时之间完成一个锻炼</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/cwu.jpg">
-						<div class="mui-media-body">
-							下午茶
-							<p class='mui-ellipsis'>在下午12时至4时之间完成一个锻炼.</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/cwan.jpg">
-						<div class="mui-media-body">
-							夜猫子
-							<p class='mui-ellipsis'>在晚上6点之后完成一个锻炼.</p>
-						</div>
-					</a>
-				</li>
-					<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/criyue.jpg">
-						<div class="mui-media-body">
-							夜以继日
-							<p class='mui-ellipsis'>在同一天三个时间段都完成锻炼.</p>
-						</div>
-					</a>
-				</li>
-			</ul>
-			<div class="title">
-				已解锁锻炼
-			</div>
-			<ul class="mui-table-view">
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/cshangbanshen.jpg">
-						<div class="mui-media-body">
-							上半身锻炼
-							<p class='mui-ellipsis'>解锁上半身锻炼</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/cquanshen.jpg">
-						<div class="mui-media-body">
-							核心部位训练
-							<p class='mui-ellipsis'>解锁核心部位训练.</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/cxiabanshen.jpg">
-						<div class="mui-media-body">
-							下半身锻炼
-							<p class='mui-ellipsis'>解锁下半身锻炼.</p>
-						</div>
-					</a>
-				</li>
-					<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/czhifan.jpg">
-						<div class="mui-media-body">
-							心肺运动
-							<p class='mui-ellipsis'>解锁心肺运动.</p>
-						</div>
-					</a>
-				</li>
-					<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/cxinfei.jpg">
-						<div class="mui-media-body">
-							脂肪燃烧
-							<p class='mui-ellipsis'>解锁脂肪燃烧.</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/cbaozha.jpg">
-						<div class="mui-media-body">
-							爆发式训练
-							<p class='mui-ellipsis'>解锁爆发式训练.</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/cliliang.jpg">
-						<div class="mui-media-body">
-							力量训练
-							<p class='mui-ellipsis'>解锁力量训练.</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="./../../../static/images/cshenzhan.jpg">
-						<div class="mui-media-body">
-							伸展运动
-							<p class='mui-ellipsis'>解锁伸展运动.</p>
-						</div>
-					</a>
-				</li>
-			</ul>
 		</div>
 	</div>
 </template>
 
+<script>
+
+			mui('.mui-scroll-wrapper').scroll({
+			    deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+			});
+
+</script>
+
 <style>
 	
-			.mui-progressbar-success span {
-				background-color: #4cd964;
-			}
-			.mui-progressbar-warning span {
-				background-color: #f0ad4e;
-			}
-			.mui-progressbar-danger span {
-				background-color: #dd524d;
-			}
-			.mui-progressbar-royal span {
-				background-color: #8a6de9;
-			}
+	.mui-progressbar-success span {
+		background-color: #4cd964;
+	}
+	.mui-progressbar-warning span {
+		background-color: #f0ad4e;
+	}
+	.mui-progressbar-danger span {
+		background-color: #dd524d;
+	}
+	.mui-progressbar-royal span {
+		background-color: #8a6de9;
+	}
 </style>
